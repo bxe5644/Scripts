6cc715dded53c193a28e2ddc8f5696a313dd5d04
Burak Engin <bxe5644@rit.edu>
2020-06-04 19:18:25 -0400

    Initial project configuration.

---
+++ src/Feather_Plucker.java
@@ -0,0 +1,38 @@
+import org.osbot.rs07.script.Script;
+import org.osbot.rs07.script.ScriptManifest;
+
+import java.awt.*;
+
+@ScriptManifest(name = "Feather Plucker", author = "Mascomorg", version = 1.0, info = "", logo = "")
+
+public class Feather_Plucker extends Script {
+
+    @Override
+    public void onStart() {
+
+        //Code here will execute before the loop is started
+
+    }
+
+    @Override
+    public void onExit() {
+
+        //Code here will execute after the script ends
+
+    }
+
+    @Override
+    public int onLoop() {
+
+        return 100; //The amount of time in milliseconds before the loop starts over
+
+    }
+
+    @Override
+    public void onPaint(Graphics2D g) {
+
+        //This is where you will put your code for paint(s)
+
+    }
+
+}
