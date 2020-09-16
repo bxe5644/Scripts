20fbaf22b01d402a0427b9cba8c269e0c7ab8b94
Burak Engin <bxe5644@rit.edu>
2020-09-16 00:19:26 -0400

    Massive changes into a fishing script.

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,11 +1,15 @@
 
 ;
 Scripts.iml,0\5\0510dce03623e543fdfd2625499838ccbcf133c9
-P
- .idea/libraries/osbot_2_5_83.xml,1\e\1e0f91a01a13cc496863570426ecc2d8f7b19e51
 S
 #.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
-G
-src/FeatherPlucker.java,a\2\a2bb5eb3ae876838630a33eb9f5bd2d3e7d01900
+K
+.idea/artifacts/Scripts.xml,5\0\504565ac2d4d7d64a29b9ebbfcaac1dfa61153ee
+D
+META-INF/MANIFEST.MF,2\e\2e8cf92205ec303e7304888d382cb6a8cf5f56ba
 ?
 src/Timing.java,5\2\5255752588629a1f4da37383b1fcd53e79029e43
+<
+src/GUI.java,a\9\a955052fd27abea410b6be4d4e0bf0624a8bc8e2
+C
+src/Overfished.java,3\e\3e559336dbb6a5a41baf70cb230f20ba294eef2c
---
+++ src/GUI.java
@@ -0,0 +1,117 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI {
+
+    private final JDialog jDialog;
+
+    private final JComboBox<String> jComboBoxLocation;
+    private final JComboBox<String> jComboBoxStyle;
+    private final JCheckBox jCheckBoxBank;
+    private final JCheckBox jCheckBoxDrop;
+
+    private boolean started;
+
+    public GUI() {
+        jDialog = new JDialog();
+        jDialog.setModal(true);
+        jDialog.setResizable(false);
+        jDialog.setTitle("Overfished");
+        jDialog.setSize(300, 400);
+        jDialog.setModalityType(Dialog.ModalityType.APPLICATION_MODAL);
+
+        JPanel jPanel = new JPanel();
+        jPanel.setLayout(null);
+
+        JLabel jLabelLocation = new JLabel("Location");
+        JLabel jLabelStyle = new JLabel("Style");
+        jComboBoxLocation = new JComboBox<>();
+        jComboBoxStyle = new JComboBox<>();
+
+        jComboBoxLocation.addItem("Barbarian Village");
+        jComboBoxLocation.addItem("Draynor");
+        jComboBoxLocation.addItem("Lumbridge Swamp");
+        jComboBoxLocation.addItem("Karamja");
+
+        jComboBoxStyle.addItem("Bait");
+        jComboBoxStyle.addItem("Fly");
+
+        jCheckBoxBank = new JCheckBox("Bank");
+        jCheckBoxDrop = new JCheckBox("Drop");
+        ButtonGroup buttonGroup = new ButtonGroup();
+
+        buttonGroup.add(jCheckBoxBank);
+        buttonGroup.add(jCheckBoxDrop);
+        jCheckBoxBank.setSelected(true);
+
+        JButton buttonStart = new JButton("Start");
+        buttonStart.setPreferredSize(new Dimension(100, 50));
+        int buttonWidth = (int) buttonStart.getPreferredSize().getWidth();
+        int buttonHeight = (int) buttonStart.getPreferredSize().getHeight();
+
+        jPanel.add(jLabelLocation);
+        jPanel.add(jLabelStyle);
+        jPanel.add(jComboBoxLocation);
+        jPanel.add(jComboBoxStyle);
+        jPanel.add(jCheckBoxBank);
+        jPanel.add(jCheckBoxDrop);
+        jPanel.add(buttonStart);
+
+        jLabelLocation.setBounds(20, 50, 50, 20);
+        jLabelStyle.setBounds(20, 120, 50, 20);
+        jComboBoxLocation.setBounds(100, 50, 150, 20);
+        jComboBoxStyle.setBounds(100, 120, 150, 20);
+        jCheckBoxBank.setBounds(20, 190, 60, 20);
+        jCheckBoxDrop.setBounds(100, 190, 60, 20);
+        buttonStart.setBounds(jDialog.getWidth() / 2 - buttonWidth / 2, 260, buttonWidth, buttonHeight);
+
+        jDialog.getContentPane().add(jPanel);
+
+        jComboBoxLocation.addActionListener(e -> {
+            String selection = jComboBoxLocation.getSelectedItem().toString();
+            if (selection.equals("Barbarian Village")) {
+                jComboBoxStyle.removeAllItems();
+                jComboBoxStyle.addItem("Bait");
+                jComboBoxStyle.addItem("Fly");
+            } else if (selection.equals("Draynor") || selection.equals("Lumbridge Swamp")) {
+                jComboBoxStyle.removeAllItems();
+                jComboBoxStyle.addItem("Bait");
+                jComboBoxStyle.addItem("Net");
+            } else {
+                jComboBoxStyle.removeAllItems();
+                jComboBoxStyle.addItem("Cage");
+                jComboBoxStyle.addItem("Harpoon");
+            }
+        });
+
+        buttonStart.addActionListener(e-> {
+            started = true;
+            close();
+        });
+    }
+
+    public String getFishingLocation() {
+        return jComboBoxLocation.getSelectedItem().toString();
+    }
+
+    public String getFishingStyle() {
+        return jComboBoxStyle.getSelectedItem().toString();
+    }
+
+    public boolean isBanking() {
+        return jCheckBoxBank.isSelected();
+    }
+
+    public boolean isStarted() {
+        return started;
+    }
+
+    public void open() {
+        jDialog.setVisible(true);
+    }
+
+    public void close() {
+        jDialog.setVisible(false);
+        jDialog.dispose();
+    }
+}
